b324126bea15806b6898e379a186463f4b16604a
ericlieu12 <ericlieu118@gmail.com>
2022-09-10 00:17:54 -0400

    Add project files.

---
+++ HealthEqualsWealth/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HealthEqualsWealth.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ HealthEqualsWealth/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthEqualsWealth.Models
+{
+  
+        public class Student
+        {
+            public int ID { get; set; }
+            public string LastName { get; set; }
+            public string FirstMidName { get; set; }
+
+        }
+    
+}
